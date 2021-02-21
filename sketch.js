7bfd0cd81c1ddf4091ba3ef13f243190866f93cf
Nithin <swapnahariega@gmail.com>
2021-02-21 15:35:22 +0530

    added the platform

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var engine, world;
 var box1, pig1;
 var backgroundImg;
 
+var platform;
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -16,7 +18,7 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,400,1200,20);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -34,6 +36,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    platform = new Ground(130,300,260,200);
 
 }
 
@@ -59,4 +63,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
